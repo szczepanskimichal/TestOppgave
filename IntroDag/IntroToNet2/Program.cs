448728de63bb3732ee4744bea5739bb7e0943d9e
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-08 16:40:54 +0200

    jeg fortsetter å øve

--- IntroDag/IntroToNet2/Program.cs
+++ IntroDag/IntroToNet2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args) //Metoda
+    {
+        Console.WriteLine("Hva er navnet ditt?"); //Wyświetlamy tekst na ekranie
+        var text = Console.ReadLine(); //Czekamy na naciśnięcie klawisza
+        Console.WriteLine(text); // Wyświetlamy tekst na ekranie
+        Console.ReadLine();
+    }
+}
