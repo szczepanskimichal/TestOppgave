e197588c00d41f441fecabe99379ab8ad290d75a
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-08 19:36:35 +0200

    Manglende Main-metode oppdaget i Program.cs, noe som forhindrer debugging. Legger til Main-metoden i neste steg.

---
+++ IntroDag/IfElseIfStatement/IfElseIfStatement/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Test
+{
+
+
+    internal class ifElse
+    {
+        int number = 5;
+        int number2 = 10;
+        bool isEqual = false;
+
+        public void EqualNumbers()
+        {
+            if (number == number2)
+            {
+                isEqual = true;
+                Console.WriteLine("Numbers are equal");
+            }
+            else
+            {
+                isEqual = false;
+                Console.WriteLine("Numbers are not equal");
+            }
+
+            if (isEqual)
+            {
+                number++;
+            }
+            else
+            {
+                number2--;
+            }
+        }
+    }
+}
