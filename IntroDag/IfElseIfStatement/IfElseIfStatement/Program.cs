c04eb39a4b9b67604b71d3cde0fccbd978ff6570
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-08 20:10:27 +0200

    • Main styrer programflyten ved å kalle metodene til ifElse-klassen. • ReadNumbers tar imot input fra brukeren. • EqualNumbers analyserer data, utfører operasjoner på tall og viser resultatene.

--- IntroDag/IfElseIfStatement/IfElseIfStatement/Program.cs
+++ IntroDag/IfElseIfStatement/IfElseIfStatement/Program.cs
@@ -1,13 +1,39 @@
-﻿
+﻿using System;
 namespace Test
 {
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ifElse test = new ifElse();
+            test.ReadNumbers(); //tall fra brukeren
+            test.EqualNumbers(); // talene er like eller ikke
+        }
+    }
 
 
     internal class ifElse
     {
-        int number = 5;
+        int number = 5; // initialiserer variabelen, default er 5
         int number2 = 10;
         bool isEqual = false;
+
+        public void ReadNumbers()
+        {
+            {
+                //Laster inn det første tallet
+                Console.WriteLine("Enter first number:");
+                number = Convert.ToInt32(Console.ReadLine()); // jeg lagrer numer fra brukeren
+                //Laster inn det andre tallet
+                Console.WriteLine("Enter second number:");
+                number2 = Convert.ToInt32(Console.ReadLine()); // jeg lagrer numer fra brukeren
+            }
+        
+            
+        }
+        
+
+        
 
         public void EqualNumbers()
         {
@@ -25,11 +51,14 @@ namespace Test
             if (isEqual)
             {
                 number++;
+               // Console.WriteLine($"Tall etter multiplikasjon {number}");
             }
             else
             {
                 number2--;
+               // Console.WriteLine($"Tall etter subtraksjon {number2}");
             }
+            Console.WriteLine($"Tall etter multiplikasjon: {number}, \n Tall etter subtraksjon: {number2}");
         }
     }
 }
