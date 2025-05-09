1fd42d1055fe3e50882a02fb51c1a21701332755
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-09 13:36:14 +0200

    Jeg flyttet initialiseringen av Random-objektet inn i metoden ThrowDice for å forbedre omfanget og unngå potensielle problemer med tilfeldighet.
    Jeg rettet en syntaksfeil i deklarasjonen av variabelen numberOfThrownNumberOne (byttet komma til semikolon).
    
    Jeg la til kall til metoden ThrowDice før definisjonen av GetRandomDice for å gjøre koden mer lesbar.

---
+++ IntroDag/RandomHobbyGenerator/kostka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+void ThrowDice()
+{
+    
+    Random random = new Random(); // Przeniesiono tutaj
+    Console.WriteLine("welcome adsondsao");
+    int numberOfThrownNumberOne = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        var diceThrow = GetRandomDice(random);
+        if (diceThrow == 1) {
+         numberOfThrownNumberOne++;
+        }
+        Console.WriteLine($"You rolled a {diceThrow}");
+    }
+    Console.WriteLine($"You rolled a 1 {numberOfThrownNumberOne} times");
+}
+ThrowDice();
+int GetRandomDice(Random random)
+{
+    int diceResult = random.Next(1, 7);
+    return diceResult;
+}
+
+
