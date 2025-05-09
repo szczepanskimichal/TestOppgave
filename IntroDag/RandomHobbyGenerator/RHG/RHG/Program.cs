5330c505d732bdb185c01017432a1bcef8035b7b
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-05-09 11:36:54 +0200

    Den nåværende Program.cs-filen inneholder et konsollprogram som genererer én tilfeldig hobby fra en liste med ti mulige og viser den i konsollen. Programmet fungerer som følger: 1. Tilfeldiggjøring av et tall: Bruker Random-klassen til å generere et heltall mellom 0 og 9. 2. Betingelsessetninger: Basert på det tegnede tallet velger programmet riktig hobby og viser den i konsollen. 3. Stopp programmet: På slutten av programmet vises Console.ReadKey(), som venter på at brukeren trykker på en tast for å forhindre at konsollen lukkes umiddelbart. Men alt dette kan gjøres med [] og en løkke, og jeg skal gjøre det i neste commit.

---
+++ IntroDag/RandomHobbyGenerator/RHG/RHG/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class RandomHobbyGenerator
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        var randomNumber = rand.Next(0, 10);
+
+        if (randomNumber == 0)
+        {
+            Console.WriteLine("Your random hobby is: Reading");
+        }
+        else if (randomNumber == 1)
+        {
+            Console.WriteLine("Your random hobby is: Painting");
+        }
+        else if (randomNumber == 2)
+        {
+            Console.WriteLine("Your random hobby is: Gardening");
+        }
+        else if (randomNumber == 3)
+        {
+            Console.WriteLine("Your random hobby is: Cooking");
+        }
+        else if (randomNumber == 4)
+        {
+            Console.WriteLine("Your random hobby is: Hiking");
+        }
+        else if (randomNumber == 5)
+        {
+            Console.WriteLine("Your random hobby is: Photography");
+        }
+        else if (randomNumber == 6)
+        {
+            Console.WriteLine("Your random hobby is: Writing");
+        }
+        else if (randomNumber == 7)
+        {
+            Console.WriteLine("Your random hobby is: Knitting");
+        }
+        else if (randomNumber == 8)
+        {
+            Console.WriteLine("Your random hobby is: Playing a musical instrument");
+        }
+        else
+        {
+            Console.WriteLine("Your random hobby is: Traveling");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
